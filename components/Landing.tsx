e0b1664aa63bd4fb91c4c999fe5c126cd375654a
Joel N. Evans <joel@joelne.tech>
2021-09-21 21:19:03 -0400

    apparently git forgot to commit a file rename

---
+++ components/Landing.tsx
@@ -0,0 +1,12 @@
+const Landing = () => {
+	return (
+		<div className='flex h-screen'>
+			<div className='pr-5 pl-2 m-auto'>
+				<h1 className='py-1.5 text-right text-3xl md:text-5xl font-bold text-purple-theme-1'>Joel N. Evans:</h1>
+				<h2 className='py-1.5 text-right text-2xl md:text-4xl font-bold text-purple-theme-2'>Full-stack Developer • Computer Science Student</h2>
+				<p className='py-1.5 text-right text-lg md:text-2xl font-semibold text-purple-theme-3'>I'm a high school CS student who's always been fascinated by technology.</p>
+			</div>
+		</div>
+	);
+};
+export default Landing;
